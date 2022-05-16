5d5e1b1d6792f2a4d57cb41e1e57e2eeb2608f37
Chi Hang Leung <cleungtech@outlook.com>
2022-05-16 01:43:17 -0400

    Refactor conditional rendering into components

--- src/components/screens/Join_Screen.js
+++ src/components/screens/Join_Screen.js
@@ -1,18 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
+import { Nav_Button } from "../models/Buttons.js";
+import { Credentials } from "../models/TextFields.js";
+import styles from "../../styles/css.js";
 import {
   Text,
   SafeAreaView,
   TouchableOpacity
 } from 'react-native';
-import { Nav_Button } from "../models/Buttons.js";
-import { Credentials } from "../models/TextFields.js";
 
-import styles from "../../styles/css.js";
+const backend_api = "https://chicken-tinder-347213.uk.r.appspot.com/api/";
 
 export const Join_Screen = ({ route }) => {
-
-  const default_url = "https://chicken-tinder-347213.uk.r.appspot.com/api/";
 
   const { user_info, flock_info } = route.params;
   const invited = flock_info !== null;
@@ -20,17 +19,16 @@ export const Join_Screen = ({ route }) => {
   const [flock_id, set_flock_id] = useState(0);
   const [flock_res, set_flock_res] = useState({});
   const [joined, set_joined] = useState(false);
-  const [error, set_error] = useState("");
+  const [network_error, set_network_error] = useState("");
 
   useEffect(() => {
     if (invited) set_flock_id(flock_info.flock_id);
   }, []);
 
   const join_flock = async () => {
-
     try {
       const response = await fetch(
-        `${default_url}flock/${flock_id}/user/${user_info.user_id}`, {
+        `${backend_api}flock/${flock_id}/user/${user_info.user_id}`, {
         method: 'POST',
         headers: {
           'Accept': 'application/json',
@@ -48,11 +46,11 @@ export const Join_Screen = ({ route }) => {
           self: json_res.self
         });
         set_joined(true);
-        set_error("");
-      } else if (response.status === 400) {
-        if (!joined) set_error("Provided Flock ID is invalid! Try again.");
-      } else {
-        if (!joined) set_error("Unable to join flock due to server error");
+        set_network_error("");
+      } else if (response.status === 400 && !joined) {
+        set_network_error("Provided Flock ID is invalid! Try again.");
+      } else if (!joined) {
+        set_network_error("Unable to join flock due to server error");
       }
     } catch (error) {
       console.error(error);
@@ -62,31 +60,62 @@ export const Join_Screen = ({ route }) => {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-      {joined ? <Text>Successfully joined {flock_res.flock_name}</Text> : null}
-      {error ? <Text>{error}</Text> : null}
-      {joined
-        ?
-        <Nav_Button
-          button_name="Go See Restaurants"
-          route="Restaurants"
-          nav_params={flock_res}
-        />
-        :
-        <>
-          <Credentials
-            inputfield="Enter Flock ID Here"
-            value={flock_id ? flock_id : ""}
-            change_function={flock_id => set_flock_id(flock_id)}
-          />
-          <TouchableOpacity
-            style={styles.button}
-            onPress={join_flock}
-            disabled={!flock_id}
-          >
-            <Text>Click here to join</Text>
-          </TouchableOpacity>
-        </>
-      }
+      <Alert_Message
+        network_error={network_error}
+        joined={joined}
+        flock_res={flock_res}
+      />
+      <Join_Flock_Form
+        joined={joined}
+        flock_id={flock_id}
+        set_flock_id={set_flock_id}
+        join_flock={join_flock}
+      />
+      <View_Restaurant_Button
+        joined={joined}
+        flock_res={flock_res}
+      />
     </SafeAreaView>
+  )
+}
+
+const Alert_Message = ({ network_error, joined, flock_res }) => {
+  if (network_error) {
+    return <Text>{network_error}</Text>
+  } else if (joined) {
+    return <Text>Successfully joined {flock_res.flock_name}</Text>
+  } else {
+    return <Text>Flock ID:</Text>;
+  }
+}
+
+const Join_Flock_Form = ({ joined, flock_id, set_flock_id, join_flock }) => {
+  if (joined) return null;
+  return (
+    <>
+      <Credentials
+        inputfield="Enter Flock ID Here"
+        value={flock_id ? flock_id : ""}
+        change_function={flock_id => set_flock_id(flock_id)}
+      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={join_flock}
+        disabled={!flock_id}
+      >
+        <Text>Click here to join</Text>
+      </TouchableOpacity>
+    </>
+  )
+}
+
+const View_Restaurant_Button = ({ joined, flock_res }) => {
+  if (!joined) return null;
+  return (
+    <Nav_Button
+      button_name="Go See Restaurants"
+      route="Restaurants"
+      nav_params={flock_res}
+    />
   )
 }
