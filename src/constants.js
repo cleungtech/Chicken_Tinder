911f75c07a0995a64d41c24baedd32e41058d45d
Chi Hang Leung <cleungtech@outlook.com>
2022-05-20 03:03:06 -0400

    Change url to production build

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 // For Production:
 export const backend_api = "https://chicken-tinder-347213.uk.r.appspot.com/api/";
-export const frontend_url = "https://chicken-tinder-cleungtech.vercel.app/";
+export const frontend_url = "https://chicken-tinder-capstone.vercel.app/";
 
 // For Developement:
 // export const frontend_url = "http://localhost:19006/";
