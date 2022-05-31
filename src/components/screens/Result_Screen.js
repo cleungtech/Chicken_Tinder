43141a2281ebd87a07a7fbcdcf358cb69a4a7caa
cleungtech <64727850+cleungtech@users.noreply.github.com>
2022-05-30 20:48:38 -0400

    Enable viewing results after all users have voted

--- src/components/screens/Result_Screen.js
+++ src/components/screens/Result_Screen.js
@@ -4,7 +4,7 @@ import { backend_api } from '../../constants';
 import { Nav_Button, Landing_Banner } from "../widgets/Buttons.js";
 import { Star_Rating } from "../widgets/Star_Rating";
 import { Display_Error } from "../widgets/Display_Error";
-import { Loading } from "../widgets/Loading"
+import { Loading } from "../widgets/Loading";
 import * as Linking from 'expo-linking';
 import styles from "../../styles/css.js";
 import {
@@ -18,10 +18,11 @@ import {
 
 export function Result_Screen({ route }) {
 
-  const { user_info, flock_info } = route.params;
+  const { flock_info } = route.params;
   const restaurants = flock_info.restaurants;
 
-  const [url_is_loading, set_url_loading] = useState(true);
+  const [interval_id, set_interval_id] = useState(null);
+  const [is_waiting, set_is_waiting] = useState(true);
   const [network_error, set_network_error] = useState("");
   const [winner_data, set_winner_data] = useState({});
 
@@ -39,10 +40,7 @@ export function Result_Screen({ route }) {
     const restaurant_ratings = Object.fromEntries(restaurants.map(r => {
       return [r.id, r];
     }));
-    console.log(most_voted_restaurants);
-    console.log('here are the ratings');
-
-    let highest_rated_restaurant = {};
+    let highest_rated_restaurant = null;
     let highest_rating = 0;
     for (const id of most_voted_restaurants) {
       if (restaurant_ratings[id].rating > highest_rating) {
@@ -50,9 +48,7 @@ export function Result_Screen({ route }) {
         highest_rating = restaurant_ratings[id].rating;
       }
     }
-
-    console.log(highest_rated_restaurant);
-    set_winner_data(highest_rated_restaurant);
+    set_winner_data(highest_rated_restaurant)
   }
 
   const get_result = async () => {
@@ -60,14 +56,17 @@ export function Result_Screen({ route }) {
       const response = await fetch(
         `${backend_api}flock/${flock_info.flock_id}/status`, {
         method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
       });
       if (response.status === 200) {
         const json_res = await response.json();
-        console.log('json_res');
-        console.log(json_res);
-        const highest_votes = json_res.most_voted_restaurants;
-        find_winner(highest_votes);
-
+        if (json_res.remaining_votes === 0) {
+          find_winner(json_res.most_voted_restaurants);
+          set_is_waiting(false);
+        }
       } else if (response.status === 400) {
         set_network_error("Unable to obtain winner due to invalid form");
       } else {
@@ -76,18 +75,27 @@ export function Result_Screen({ route }) {
     } catch (error) {
       set_network_error("Fetch request failed.");
       console.error(error);
+      alert(error.toString());
     } finally {
-      set_url_loading(false);
       fade_in();
     }
   };
 
   useEffect(() => {
-    get_result();
-  }, []);
+    if (is_waiting) {
+      get_result();
+      const interval_id = setInterval(get_result, 1000);
+      set_interval_id(interval_id);
+    } else {
+      clearInterval(interval_id);
+    }
+  }, [is_waiting]);
 
-  if (url_is_loading) return <Loading />
+  if (is_waiting) return <Loading message="Waiting for others..." />
   if (network_error) return <Display_Error network_error={network_error} />
+  
+
+  clearInterval(interval_id);
   return (
     <SafeAreaView style={styles.result_container}>
       <StatusBar style="auto" />
@@ -97,7 +105,6 @@ export function Result_Screen({ route }) {
 }
 
 function Winning_Card({ shop_data }) {
-  console.log(shop_data);
 
   function go_to_maps(coords) {
     const map_url =
@@ -115,39 +122,38 @@ function Winning_Card({ shop_data }) {
 
   return (
     <SafeAreaView style={styles.header_container}>
-      {/* <Landing_Banner/> */}
-      <View style={styles.results_card}>
-        <Text style={styles.winner_name}>{shop_data.name}</Text>
-        <Image
-          style={styles.image_rounded}
-          source={{ uri: shop_data.image_url }}
-        />
-        <Star_Rating star_num={shop_data.rating} shop_id={shop_data.id}></Star_Rating>
-        <Text style={styles.winner_button_header}>
-          Review Count: {shop_data.review_count}
-        </Text>
-        <TouchableOpacity
-          onPress={() => go_to_maps(shop_data.coordinates)}
-          style={styles.maps_button}
-          activeOpacity={0.5}
-        >
-          <Text style={styles.winner_button_header}>Get Directions: </Text>
-          <Text>{shop_data.location.display_address[0]}</Text>
-          <Text>{shop_data.location.display_address[1]}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => go_to_phone(shop_data.phone)}
-          style={styles.contact_button}
-          activeOpacity={0.5}
-        >
-          <Text style={styles.winner_button_header}>Contact: </Text>
-          <Text>{shop_data.display_phone}</Text>
-        </TouchableOpacity>
-        <Nav_Button
-          button_name="Start Over"
-          route="Login"
-        />
-      </View>
+    <View style={styles.results_card}>
+      <Text style={styles.winner_name}>{shop_data.name}</Text>
+      <Image
+        style={styles.image_rounded}
+        source={{ uri: shop_data.image_url }}
+      />
+      <Star_Rating star_num={shop_data.rating} shop_id={shop_data.id}></Star_Rating>
+      <Text style={styles.winner_button_header}>
+        Review Count: {shop_data.review_count}
+      </Text>
+      <TouchableOpacity
+        onPress={() => go_to_maps(shop_data.coordinates)}
+        style={styles.maps_button}
+        activeOpacity={0.5}
+      >
+        <Text style={styles.winner_button_header}>Get Directions: </Text>
+        <Text>{shop_data.location.display_address[0]}</Text>
+        <Text>{shop_data.location.display_address[1]}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => go_to_phone(shop_data.phone)}
+        style={styles.contact_button}
+        activeOpacity={0.5}
+      >
+        <Text style={styles.winner_button_header}>Contact: </Text>
+        <Text>{shop_data.display_phone}</Text>
+      </TouchableOpacity>
+      <Nav_Button
+        button_name="Start Over"
+        route="Login"
+      />
+    </View>
     </SafeAreaView>
   );
 }
