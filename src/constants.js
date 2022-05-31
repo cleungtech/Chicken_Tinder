e7d9922446c94f1988ece49a322cee928430da19
cleungtech <64727850+cleungtech@users.noreply.github.com>
2022-05-30 20:22:57 -0400

    Update urls for local developement

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,9 @@
 // For Production:
 export const backend_api = "https://chicken-tinder-347213.uk.r.appspot.com/api/";
-export const frontend_url = "https://chicken-tinder-capstone.vercel.app/";
-export const mobile_url = "chicken-tinder://";
+// export const frontend_url = "https://chicken-tinder-capstone.vercel.app/";
+// export const mobile_url = "chicken-tinder://";
 
 // For Developement:
-// export const frontend_url = "http://localhost:19006/";
-// export const mobile_url = "exp://192.168.0.112:19000/--/";
+const localhost = "192.168.0.107";
+export const frontend_url = `http://${localhost}:19006`;
+export const mobile_url = `exp://${localhost}:19000/--/`;
